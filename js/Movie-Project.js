ec4af02ea3ceee58703322818367b4c2a6212523
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-04-20 12:29:53 -0500

    Feat:updates

--- js/Movie-Project.js
+++ js/Movie-Project.js
@@ -4,26 +4,46 @@
 var apitest = fetch(movieAPI)
 console.log(apitest);
 
-fetch(movieAPI)
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
+console.log(fetch(movieAPI));
+
+fetch(movieAPI).then((response)=>{return response.json()})
+	.then((jsonData)=>{
+		console.log(jsonData);
+		return jsonData.results;
+	})
+
+
+
+// MOVIE TITLE
+var movieTitles = fetch(movieAPI).then((response)=>{return response.json()})
+	.then((jsonData)=> {
+		console.log(jsonData);
+		jsonData.forEach((movie)=>console.log(`${movie.title} ${movie.genre}`))
+	})
+console.log(movieTitles);
+
+//
+
+// var titles = movieAPI.filter(function(movies){
+// 	return movies.title;
+// })
+// console.log(titles);
+
+$(document).ready(function(){
+	$(".loadingdiv").css("display","none")
+
+
+	$(window).load(function(){
+		$("body").fadeIn(5);
+	})
+
+	// var currentMovies = $("#currentMovies").html(<ul>title</ul>)
+	//
 
 
 
 
-// $(document).ready(function(){
-// 	$(".loadingdiv").css("display","none")
-//
-//
-// 	$(window).load(function(){
-// 		$("body").fadeIn(5);
-// 	})
-//
-//
-//
-//
-//
-//
-//
-//
-// });
+
+
+
+});
