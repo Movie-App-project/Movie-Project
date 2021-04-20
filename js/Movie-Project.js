e91b005fcb786dd43bc5e38b4e414c393002f08b
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-20 15:29:00 -0500

    lets get it

--- js/Movie-Project.js
+++ js/Movie-Project.js
@@ -1,13 +1,67 @@
-"use strict";
+"use strict"
 
-console.log(fetch(movieAPI));
+// KEY //
 
-fetch(movieAPI).then((response)=>{return response.json()})
-    .then((jsonData)=>{
+fetch(movieAPI).then((response) => {
+    return response.json()
+})
+    .then((jsonData) => {
         console.log(jsonData);
-    return jsonData.results;
+        return jsonData.results;
+    })
+
+
+// MOVIE TITLE
+var movieTitles = fetch(movieAPI).then((response) => {
+    return response.json()
 })
-    .then((results)=>{
-    results.forEach((movies)=>console.log(`${movies.title} ${movies.genre}`))
-})
-//     .catch(error => console.log('Error getting films.', error));
+    .then((jsonData) => {
+        console.log(jsonData);
+        jsonData.forEach((movie) => console.log(`${movie.title} ${movie.genre}`))
+    })
+console.log(movieTitles);
+
+var titleArr
+
+$(document).ready(function () {
+    titleArr = []
+    // $(window).ready(function () {
+    //     $("body").fadeIn(1000);
+    // })
+    fetch(movieAPI).then((response) => {
+        // $(".loadingdiv").css("display", "none")
+        $(".loadingdiv").fadeOut(1000)
+        return response.json()
+    })
+        .then((jsonData) => {
+            console.log(jsonData);
+            jsonData.forEach(function(movie) {
+                // titleArr.push(${movie.title} ${movie.genre})
+                // console.log(renderMovies(movie));
+                $("#currentMovies").append(`<div>${renderMovies(movie)}</div>`)
+                // console.log(titleArr)
+            })
+        })
+    $("#currentMovies").append(`<div>${titleArr}</div>`)
+    console.log(titleArr)
+});
+
+
+function renderMovies(movie) {
+    var html = '<div class="card">';
+    html += '<h6>' + movie.title + '</h6>';
+    html += '<p>' + movie.rating + '</p>';
+    html += '</div>';
+    return html;
+    console.log(html)
+}
+// renderMovies()
+
+
+function renderCoffees(coffees) {
+    var html = '';
+    for (var i = 0; i < coffees.length; i++) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
