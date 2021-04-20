a1e9049371e7b9ebc5200267376c34db7392d222
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-20 12:37:22 -0500

    stuff'

--- js/Movie-Project.js
+++ js/Movie-Project.js
@@ -1,13 +1,8 @@
 "use strict";
 
-console.log(fetch(movieAPI));
-
 fetch(movieAPI).then((response)=>{return response.json()})
     .then((jsonData)=>{
         console.log(jsonData);
     return jsonData.results;
-})
-    .then((results)=>{
-    results.forEach((movies)=>console.log(`${movies.title} ${movies.genre}`))
-})
-//     .catch(error => console.log('Error getting films.', error));
+});
+
